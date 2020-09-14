34c137fc3f5c2fca7b7ee0c64c66dac607326926
Dan de Havilland <dan@dandehavilland.com>
2020-09-14 08:51:15 +0200

    Adding matchability to emails

--- src/main/java/co/livil/workapi/model/Integration.kt
+++ src/main/java/co/livil/workapi/model/Integration.kt
@@ -7,5 +7,6 @@ import moe.banana.jsonapi2.Resource
 @JsonApi(type = "integration")
 data class Integration (
     @field:Json(name = "provider") var provider: String = "",
+    @field:Json(name = "provider_id") var providerId: String? = null,
     @field:Json(name = "media_type") var mediaType: String = ""
 ) : Resource()
