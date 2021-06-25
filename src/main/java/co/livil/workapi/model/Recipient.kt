ee20f6cc68e557807335fdcd62c3e074fb5e0c71
Dan de Havilland <dan@dandehavilland.com>
2021-06-25 08:22:17 +0200

    Utility method to create Recipient from an email address string

--- src/main/java/co/livil/workapi/model/Recipient.kt
+++ src/main/java/co/livil/workapi/model/Recipient.kt
@@ -7,4 +7,15 @@ import com.squareup.moshi.JsonClass
 data class Recipient (
     @Json(name = "name") var name: String = "",
     @Json(name = "address") var address: String = ""
-)
+) {
+    companion object {
+        fun fromString(str: String): Recipient {
+            return if (str.matches(".* <.*@.*>".toRegex())) {
+                val segments = str.split("<", ">").filter { it.isNotEmpty() }
+                Recipient(name = segments[0].trim(), address = segments[1].trim())
+            } else {
+                Recipient(address = str.trim())
+            }
+        }
+    }
+}
